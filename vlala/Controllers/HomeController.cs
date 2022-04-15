8740095fb0aea65311306fa6e77ace9380f76f44
Georgina Bacskó <bacsko.georgina@gmail.com>
2022-04-15 20:01:53 +0200

    kereso

--- vlala/Controllers/HomeController.cs
+++ vlala/Controllers/HomeController.cs
@@ -7,24 +7,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using MotherHood.Models;
 using Microsoft.AspNetCore.Authorization;
+using MotherHood.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace MotherHood.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+            ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? keresMegye, string keresNev)
         {
-            return View();
-        }
+            var felhasznalok = _context.Users.Select(x => x);
 
+            if (keresMegye != null)
+            {
+                felhasznalok = felhasznalok.Include(user => user.Megye).Where(felhasznalo => felhasznalo.Megye.Id == keresMegye);
+            }
+
+            if (!string.IsNullOrEmpty(keresNev))
+            {
+                felhasznalok = felhasznalok.Include(user => user.Megye).Where(felhasznalo => felhasznalo.firstName.Contains(keresNev) || felhasznalo.lastName.Contains(keresNev));
+            }
+
+            List<Megye> megyelista = _context.Megye.ToList();
+
+            UsersSearchViewModel modelkereseshez = new UsersSearchViewModel();
+            modelkereseshez.Megye = megyelista.Select(x => new SelectListItem() { Text = x.Nev, Value = x.Id.ToString() }).ToList();
+            modelkereseshez.keresNev = keresNev;
+            modelkereseshez.applicationUsers = await felhasznalok.ToListAsync();
+
+
+
+
+            return View(modelkereseshez);
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ vlala/Models/UsersSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotherHood.Models
+{
+    public class UsersSearchViewModel
+    {
+        public string keresNev { get; set; }
+
+        public int keresMegye { get; set; }
+
+
+        public List<ApplicationUser> applicationUsers { get; set; }
+        public List<SelectListItem> Megye { get; set; }
+     }
+ }
